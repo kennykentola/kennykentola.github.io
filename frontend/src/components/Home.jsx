c0ba59132ef07cf3cf7d658f236ee0420d319b70
kennykentola <peterkehindeademola9@gmail.com>
2025-03-01 18:46:55 +0100

    Updated portfolio website

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,32 +1,28 @@
-
-
- import React from "react";
-import "./Home.css";
-import Contact from "./Contact";
-import Skills from "./Skills";
-import EducationExperience from "./EducationExperience";
-import About from "./About";  
-import HeroSection from "./HeroSection";
-import Projects from "./Projects";
-import Testimonials from "./Testimonials";
-
+import React from "react";
+import { Helmet } from "react-helmet-async";
 
 function Home() {
   return (
     <div className="home-container">
-      {/* Navigation */}
-     
-      
-    <HeroSection/>
-    <About/>
-    <Skills/>
-    <Projects/>
-    <EducationExperience/>
-    <Testimonials/>
-    <Contact />
-       {/* <Footer />  */}
+      <Helmet>
+        <title>KennyKentola Portfolio</title>
+        <meta property="og:title" content="KennyKentola Portfolio" />
+        <meta property="og:description" content="Welcome to my portfolio website!" />
+        <meta property="og:image" content="https://your-image-url.com/portfolio-image.png" />
+        <meta property="og:url" content="https://kennykentola.vercel.app" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Helmet>
+
+      {/* Page Content */}
+      <HeroSection />
+      <About />
+      <Skills />
+      <Projects />
+      <EducationExperience />
+      <Testimonials />
+      <Contact />
     </div>
   );
-} 
+}
 
 export default Home;
