d7423ee25b81c4a55484fdd0d4adc4f67c365224
kennykentola <peterkehindeademola9@gmail.com>
2025-03-03 04:47:31 +0100

    Fixed Helmet error

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -8,19 +8,19 @@ import Projects from "./Projects";//
 import EducationExperience from "./EducationExperience";//
 import Testimonials from "./Testimonials";//
 import Contact from "./Contact";//
-// import { Helmet } from "react-helmet-async";//
+import { Helmet } from "react-helmet-async";//
 
 function Home() {
   return (
     <div className="home-container">
-      {/* <Helmet>
+      <Helmet>
         <title>KennyKentola Portfolio</title>
         <meta property="og:title" content="KennyKentola Portfolio" />
         <meta property="og:description" content="Welcome to my portfolio website!" />
         <meta property="og:image" content="https://your-image-url.com/portfolio-image.png" />
         <meta property="og:url" content="https://kennykentola.vercel.app" />
         <meta name="twitter:card" content="summary_large_image" />
-      </Helmet> */}
+      </Helmet>
 
       {/* Page Content */}
       <HeroSection />
